34233703387684558b6ee717fef4d8823acfc531
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-03-23 10:10:47 -0300

    first commit

---
+++ components/layout/Toolbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <VToolbar
+    clipped-left
+    app
+    fixed
+    :color="toolbarColor"
+  >
+    <VBtn icon @click.stop="drawer = !drawer">
+      <VIcon :color="elementsColor">
+        mdi-chevron-left
+      </VIcon>
+    </VBtn>
+
+    <VSpacer />
+
+    <img
+      src="../../static/logos/logo-azul.svg"
+      alt="Tigre Performance"
+      width="auto"
+      height="90%"
+    >
+
+    <VSpacer />
+
+    <VBtn icon @click.stop="drawer = !drawer">
+      <VIcon :color="elementsColor">
+        mdi-menu
+      </VIcon>
+    </VBtn>
+  </VToolbar>
+</template>
+
+<script>
+export default {
+  props: {
+    toolbarColor: { type: String, default: 'white' },
+    elementsColor: { type: String, default: 'primary' } // ,
+    // imageName: { type: String, default: 'logo-azul' }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <VApp>
+    <Toolbar />
+
+    <VContent>
+      <VContainer grid-list-md>
+        <nuxt />
+      </VContainer>
+    </VContent>
+  </VApp>
+</template>
+
+<script>
+import Toolbar from '@/components/layout/Toolbar'
+export default {
+  components: { Toolbar }
+}
+</script>
